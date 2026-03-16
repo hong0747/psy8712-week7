c0257c4522a7fa78a20ad5a4094396fd1d6e8e4d
Julia Hong <hong0747@umn.edu>
2026-03-16 12:35:41 -0500

    Fixed order of male and female

--- R/week7.R
+++ R/week7.R
@@ -7,8 +7,8 @@ library(ggplot2)
 
 # Data Import and Cleaning
 week7_tbl <- read_csv("../data/week3.csv", col_names = TRUE, col_types = cols(timeStart = col_datetime("%F %h:%M:%S")), show_col_types = TRUE) %>%
-  mutate(gender = c("M" = "Male", "F" = "Female")[gender]) %>%
-  mutate(condition = c("A" = "Block A", "B" = "Block B", "C" = "Control")[condition]) %>%
+  mutate(gender = factor(gender, levels = c("M", "F"), labels = c("Male", "Female"))) %>%
+  mutate(condition = factor(condition, levels = c("A", "B", "C"), labels = c("Block A", "Block B", "Control"))) %>%
   filter(q6 == 1) %>%
   mutate(q6 = NULL) %>%
   mutate(timeSpent = as.numeric(timeEnd - timeStart, units = "mins"))
