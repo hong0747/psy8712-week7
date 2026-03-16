b7fda816d1cdb9da76eeca9feae3796b4ddb05c3
Julia Hong <hong0747@umn.edu>
2026-03-16 12:04:09 -0500

    Data import and cleaning

--- R/week7.R
+++ R/week7.R
@@ -6,12 +6,12 @@ library(tidyverse)
 
 
 # Data Import and Cleaning
-
-
-
-
-
-
+week7_tbl <- read_csv("../data/week3.csv", col_names = TRUE, col_types = cols(timeStart = col_datetime("%F %h:%M:%S")), show_col_types = TRUE) %>%
+  mutate(gender = c("M" = "Male", "F" = "Female")[gender]) %>%
+  mutate(condition = c("A" = "Block A", "B" = "Block B", "C" = "Control")[condition]) %>%
+  filter(q6 == 1) %>%
+  mutate(q6 = NULL) %>%
+  mutate(timeSpent = as.numeric(timeEnd - timeStart, units = "mins"))
 
 
 
